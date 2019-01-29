22bf6e47ee6d4fef73c0d083e80f551e1bbf401b
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2019-01-29 10:14:14 +0100

    Make news photo optional

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         <ul class="list-reset">
             @foreach($news as $article)
                 <div class="w-full lg:flex p-2">
-                    <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('{{ Storage::url($article->photo_url) }}')" title="News photo">
+                    <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('{{ Storage::disk('public')->url($article->photo_url) }}')" title="News photo">
                     </div>
                     <div class="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
                         <div class="mb-8">
